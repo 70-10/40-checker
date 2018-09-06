116edc55eb4b2e77aeb65c89fa21a3561f1a728c
70_10 <7010.dev@gmail.com>
2018-09-06 14:05:58 +0900

    [mod] state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { text: "" };
+    this.changeText = this.changeText.bind(this);
+  }
+
+  changeText(e) {
+    this.setState({ text: e.target.value });
+  }
+
   render() {
     return (
       <section className="hero is-fullheight">
@@ -8,14 +19,19 @@ class App extends Component {
           <div className="container has-text-centered">
             <div className="columns">
               <div className="column">
-                <h2 className="title">0</h2>
+                <h2 className="title">{this.state.text.length}</h2>
               </div>
             </div>
             <div className="columns">
               <div className="column">
                 <div className="field">
                   <div className="control">
-                    <input type="text" className="input is-large" />
+                    <input
+                      type="text"
+                      className="input is-large"
+                      value={this.state.text}
+                      onChange={this.changeText}
+                    />
                   </div>
                 </div>
               </div>
